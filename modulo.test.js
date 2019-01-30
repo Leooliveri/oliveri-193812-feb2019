1571aef9431307ee3f891834e847fe48ac906ff6
Leooliveri <leonardo.oliveri@studenti.unitn.it>
2019-01-30 17:30:30 +0100

    Fourth commit

--- modulo.test.js
+++ modulo.test.js
@@ -1,11 +1,11 @@
 const modulo = require('./modulo');
 
-test('modulo(0) dovrebbe tornare 0', () => {
+test('modulo(0) dovrebbe ritornare 0', () => {
     expect(modulo(0)).toBe(0)
 });
-test('modulo(100) dovrebbe tornare 100', () => {
+test('modulo(100) dovrebbe ritornare 100', () => {
     expect(modulo(100)).toBe(100)
 });
-test('modulo(-100) dovrebbe tornare -100', () => {
+test('modulo(-100) dovrebbe ritornare -100', () => {
     expect(modulo(-100)).toBe(100)
 });
